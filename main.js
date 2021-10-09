57c8eb35bf84c088f1b6b71e7a057e7df9cd63bb
ryunosinfx <ryunosinfxxul@gmail.com>
2021-10-10 04:47:45 +0900

    カラーマネジメント違いを回避するためグレイスケールへ対応。

--- main.js
+++ main.js
@@ -186,9 +186,12 @@ class Base64Util {
 		const bs = u8a ? Base64Util.u8a2bs(u8a) : null;
 		return bs ? btoa(bs) : null;
 	}
+	static u8a2b64u(u8a) {
+		const b64 = u8a ? Base64Util.u8a2b64(u8a) : null;
+		return b64 ? Base64Util.toB64u(b64) : null;
+	}
 	static s2u8a(s) {
-		const d = Base64Util.to64(s);
-		return Base64Util.b64ToU8a(d);
+		return te.encode(s);
 	}
 	static b64uToAb(b) {
 		const d = Base64Util.toB64(b);
@@ -369,17 +372,6 @@ class Cryptor {
 		return new Uint8Array(decryptedData);
 	}
 }
-// if (!('createImageBitmap' in window)) {
-// 	window.createImageBitmap = async (blob) => {
-// 		return new Promise((resolve, reject) => {
-// 			let img = document.createElement('img');
-// 			img.addEventListener('load', () => {
-// 				resolve(this);
-// 			});
-// 			img.src = URL.createObjectURL(blob);
-// 		});
-// 	};
-// }
 const imgRe = /^image\/.+|application\/octet-stream/;
 class ImageProcessor {
 	constructor() {
@@ -425,7 +417,7 @@ class ImageProcessor {
 		const lenData = data.length;
 		const ab = new ArrayBuffer(LEN_LENGTH + LEN_TYPE + lenData + LEN_CRC);
 		const view = new DataView(ab);
-
+		new Uint8Array(ab).fill(0);
 		let pos = LEN_LENGTH;
 		view.setUint32(0, lenData);
 		const lenType = type.length;
@@ -517,17 +509,20 @@ class View {
 	}
 }
 class ImageBuilder {
-	constructor(fileId, imageids, sizeInputId, sizeExpected, pwId, sigId, sizeId, view, logElmId, redoBtnId) {
+	constructor(fileId, imageids, sizeInputId, sizeExpected, pwId, sigId, sizeId, view, logElmId, redoBtnId, isGrascaleId) {
 		this.data = {};
 		this.isLoading = false;
 		const inputSize = v.gid(sizeInputId);
 		const redoBtnElm = v.gid(redoBtnId);
 		const logElm = v.gid(logElmId);
+		const isGrascaleElm = v.gid(isGrascaleId);
 		inputSize.value = 900;
 		const sizeFunc = (event) => {
 			const inputSize = event.target;
 			const size = inputSize.value;
-			const bits = (size * size * 3 - 3) * 3 - 280;
+			const mtrix = size * size * 1;
+			const bits = (isGrascaleElm.checked ? (mtrix - 2) * 4 : (mtrix * 3 - 3) * 3 - 280) - 17;
+			console.log(bits + '/' + mtrix);
 			const sizeExpectedElm = v.gid(sizeExpected);
 			sizeExpectedElm.textContent = '' + bits.toLocaleString() + '';
 		};
@@ -547,10 +542,13 @@ class ImageBuilder {
 					sigElm.textContent = await Base64Util.sig(u8a);
 					sizeElm.textContent = u8a.length.toLocaleString();
 				}
-				await this.f2is(fileId, imageids, file.name, inputSize.value, passwd.value, logElm);
+				await this.f2is(fileId, imageids, file.name, inputSize.value, passwd.value, logElm, isGrascaleElm.checked);
 			}
 			view.heideLoading();
 		};
+		v.ael(isGrascaleId, 'change', () => {
+			sizeFunc({ target: inputSize });
+		});
 		const funcWrapped = async (event) => {
 			const evt = event.type === 'change' ? event : this.lastEvent;
 			this.lastEvent = evt;
@@ -585,7 +583,18 @@ class ImageBuilder {
 			imgElm.src = '';
 		}
 	}
-	async f2is(fileId, imageids, fileName, size, passwd, logElm) {
+	async f2is(fileId, imageids, fileName, size, passwd, logElm, isGrascale = false) {
+		const imageElms = [];
+		const promises = isGrascale
+			? await this.tobeGrayScale(fileId, imageids, fileName, size, passwd, imageElms, logElm)
+			: await this.tobeRGB(fileId, imageids, fileName, size, passwd, imageElms, logElm);
+		const results = await Promise.all(promises);
+		for (let duri of results) {
+			const imgElm = imageElms.shift();
+			imgElm.src = duri;
+		}
+	}
+	async tobeRGB(fileId, imageids, fileName, size, passwd, imageElms, logElm) {
 		const MaxPixcelParImg = isNaN((size + '') * 1) ? 900 : size * 1;
 		const MAX_bytes4 = MaxPixcelParImg * MaxPixcelParImg * 4;
 		const MAX_bytes3 = MaxPixcelParImg * MaxPixcelParImg * 3;
@@ -602,12 +611,11 @@ class ImageBuilder {
 		}
 		console.log('jast dataSize:' + len + '/allowSize:' + (MAX_bytes3 - 4) * 4 + '/pixcel:' + size);
 		const lenWithHole = len + 4 * 4;
-		const count = Math.ceil(lenWithHole / MAX_bytes3);
 		this.init(imageids);
 		const packLen = MAX_bytes3 - 3;
+		const imgCount = Math.ceil(lenWithHole / packLen);
 		const promises = [];
-		const imageElms = [];
-		for (let i = 0; i < count; i++) {
+		for (let i = 0; i < imgCount; i++) {
 			const imageId = imageids[i];
 			imageElms.push(v.gid(imageId));
 			const start = i * packLen;
@@ -641,11 +649,83 @@ class ImageBuilder {
 			}
 			promises.push(ip.getUriFomBMD({ data: a, width: MaxPixcelParImg, height: MaxPixcelParImg }, logElm));
 		}
-		const results = await Promise.all(promises);
-		for (let duri of results) {
-			const imgElm = imageElms.shift();
-			imgElm.src = duri;
+		return promises;
+	}
+	async tobeGrayScale(fileId, imageids, fileName, size, passwd, imageElms, logElm) {
+		const MaxPixcelParImg = isNaN((size + '') * 1) ? 900 : size * 1;
+		const MAX_pixcelNum = MaxPixcelParImg * MaxPixcelParImg * 1;
+		const MAX_bytes4 = MAX_pixcelNum * 4;
+		const MAX_bytes2 = MAX_pixcelNum * 2;
+		const dataUri = this.data[fileId];
+		this.fileName = fileName;
+		const fnb64 = Base64Util.to64(fileName);
+		const [meta, data] = dataUri.split(COMMA);
+		const dataE = passwd ? await Cryptor.encrypt(passwd, Base64Util.b64ToU8a(data)) : data;
+		const dataPre = [fnb64, meta].join(COMMA) + ',';
+		const u8as = [Base64Util.s2u8a(dataPre)];
+		const dataEs = passwd ? dataE.split('/') : [dataE];
+		const firstDataE = passwd ? Base64Util.toB64(dataEs[0]) : dataEs[0];
+		u8as.push(Base64Util.b64ToU8a(firstDataE));
+		if (dataEs.length > 1) {
+			u8as.push(Base64Util.b64ToU8a(Base64Util.toB64(dataEs[1])));
 		}
+		const u8asLen = u8as.length;
+		const len = u8as[0].length + u8as[1].length + (u8asLen > 2 ? u8as[2].length : 0) + u8asLen * 3;
+		const nu16a = new Uint16Array(len);
+		nu16a.fill(0);
+		let count = 0;
+		for (const u8a of u8as) {
+			const lenU8a = u8a.length;
+			for (let i = 0; i < lenU8a; i++) {
+				nu16a[count + i] = u8a[i];
+			}
+			count += lenU8a;
+			nu16a.set([256], count);
+			count += 1;
+		}
+		const END_TAIL_LEN = 4 * 4;
+		const GSMax = (MAX_bytes2 - 8) * 4;
+		const Limit = MAX_pixcelNum - 3;
+		if (len / 4 > Limit) {
+			alert('too fat data! dataSize:' + len + '/allowSize:' + GSMax + '/pixcel:' + size);
+			return;
+		}
+		console.log('jast dataSize:' + len + '/allowSize:' + GSMax + '/pixcel:' + size);
+		const lenWithHole = len + END_TAIL_LEN;
+		const packLen = MAX_pixcelNum - 4;
+		const imgCount = Math.ceil(lenWithHole / packLen);
+		this.init(imageids);
+		const promises = [];
+		for (let i = 0; i < imgCount; i++) {
+			const imageId = imageids[i];
+			imageElms.push(v.gid(imageId));
+			const start = i * packLen;
+			const end = start + packLen;
+			const sub = nu16a.subarray(start, end);
+			const a = new Uint8ClampedArray(MAX_bytes4);
+			a.fill(0);
+			const initCount = 2;
+			for (let j = initCount; j < MAX_pixcelNum; j++) {
+				const startIndex = j * 4;
+				const mainIndex = j - 2;
+				const main = sub[mainIndex];
+				if (main === undefined) {
+					continue;
+				} else if (main === 256) {
+					a[startIndex] = 0;
+					a[startIndex + 1] = 0;
+					a[startIndex + 2] = 0;
+					a[startIndex + 3] = 0;
+				} else {
+					a[startIndex] = main;
+					a[startIndex + 1] = main;
+					a[startIndex + 2] = main;
+					a[startIndex + 3] = 255;
+				}
+			}
+			promises.push(ip.getUriFomBMD({ data: a, width: MaxPixcelParImg, height: MaxPixcelParImg }, logElm));
+		}
+		return promises;
 	}
 }
 class FileBuilder {
@@ -702,55 +782,34 @@ class FileBuilder {
 	}
 	async bfDL(passwd, logElm) {
 		const u8as = [];
+		let isGrascale = false;
 		logElm.textContent = logElm.textContent + '\n' + 'bfDL 01';
-		for (let id of fileIds) {
-			const b64d = this.data[id];
+		const newMap = {};
+		for (const id of fileIds) {
+			newMap[id] = JSON.parse(JSON.stringify(this.data[id]));
+		}
+		for (const id of fileIds) {
+			const b64d = newMap[id];
 			if (!b64d) {
 				continue;
 			}
 			logElm.textContent = logElm.textContent + '\n' + 'bfDL 01a ' + b64d.substring(b64d.length - 40) + ' ' + b64d.substr(Math.floor(b64d.length / 3), 40) + ' ';
 			const ab = await ip.loadAbFromPngDataUri(b64d, logElm);
 			const u8a = new Uint8Array(ab);
-			const len = u8a.length;
-			const nlen = Math.floor((len / 4) * 3);
-			const nU8a = new Uint8Array(nlen);
-			const a = [];
-			for (let i = 4; i < len; i++) {
-				const byte = u8a[i];
-				a.push(byte);
-				if (i % 4 === 3) {
-					continue;
-				}
-				const k = i - Math.floor(i / 4);
-				nU8a[k] = byte;
+			let count = 0;
+			for (let i = 4; i < 8; i++) {
+				count += u8a[i];
 			}
-			const str = td.decode(nU8a);
-			const as = a.join(',');
-			logElm.textContent = logElm.textContent + '\n' + 'bfDL 01b ' + str.substr(Math.floor(0 / 3), 40) + ' ' + as.substr(Math.floor(0 / 3), 40) + ' ';
-			u8as.push(nU8a.subarray(3));
+			isGrascale = isGrascale || count === 0;
+			isGrascale ? this.doAsGS(u8a, u8as, logElm) : this.doAsRGB(u8a, u8as, logElm);
 		}
-		logElm.textContent = logElm.textContent + '\n' + 'bfDL 02';
-		const u8a = Base64Util.joinU8as(u8as);
-		// const str2 = Base64Util.u8a2bs(u8a);
-		logElm.textContent = logElm.textContent + '\n' + 'bfDL 03';
-		const str = td.decode(u8a);
-		logElm.textContent = logElm.textContent + '\n' + 'bfDL 04';
-		const tokens = str.split(',');
-		if (tokens.length < 3) {
-			console.log('str:' + str);
-			alert('invalid data! Empty!');
-			return null;
-		}
-		logElm.textContent = logElm.textContent + '\n' + 'bfDL 05';
-		const fnb64 = tokens[0];
-		const type = tokens[1];
-		const b64 = tokens[2];
 
+		logElm.textContent = logElm.textContent + '\n' + 'bfDL 02';
+		const { fnb64, type, b64 } = isGrascale ? this.finishGS(u8as, logElm) : this.finishRGB(u8as, logElm);
 		logElm.textContent = logElm.textContent + '\n' + 'bfDL 06';
-		// logElm.textContent = logElm.textContent + '\n' + b64;
 		const b64a = passwd ? Base64Util.u8a2b64(await Cryptor.decrypt(passwd, b64)) : b64;
 		if (!Base64Util.isB64(b64a)) {
-			console.log('str:' + str);
+			console.log('b64a:' + b64a);
 			alert('invalid data! Crypted Data!');
 			return null;
 		}
@@ -760,6 +819,94 @@ class FileBuilder {
 		logElm.textContent = logElm.textContent + '\n' + 'bfDL 08';
 		FL.dl(fileName, dataUri, null, true);
 		return Base64Util.b64ToU8a(b64a);
+	}
+	doAsRGB(sourceU8a, u8as, logElm) {
+		const len = sourceU8a.length;
+		const nlen = Math.floor((len / 4) * 3);
+		const nU8a = new Uint8Array(nlen);
+		const a = [];
+		for (let i = 4; i < len; i++) {
+			const byte = sourceU8a[i];
+			a.push(byte);
+			if (i % 4 === 3) {
+				continue;
+			}
+			const k = i - Math.floor(i / 4);
+			nU8a[k] = byte;
+		}
+		const str = td.decode(nU8a);
+		const as = a.join(',');
+		logElm.textContent = logElm.textContent + '\n' + 'doAsRGB ' + str.substr(Math.floor(0 / 3), 40) + ' ' + as.substr(Math.floor(0 / 3), 40) + ' ';
+		u8as.push(nU8a.subarray(3));
+	}
+	doAsGS(sourceU8a, u8as, logElm) {
+		const len = sourceU8a.length;
+		const nU8a = new Uint8Array(len);
+		const a = [];
+		let k = 0;
+		let lastK = 0;
+		const b = [];
+		for (let i = 8; i < len; i++) {
+			if (i % 4 !== 1) {
+				continue;
+			}
+			const main = sourceU8a[i]; //main
+			const alpha = sourceU8a[i + 2]; //alpha
+			if (alpha === 0) {
+				lastK !== k ? b.push(nU8a.subarray(lastK, k)) : '';
+				lastK = k;
+			} else {
+				nU8a[k] = main;
+				k++;
+			}
+		}
+		console.log('doAsGS sourceU8a len:' + len + '/k:' + k + '/lastK:' + lastK);
+		lastK !== k ? b.push(nU8a.subarray(lastK, k)) : '';
+		const str = td.decode(nU8a);
+		const as = a.join(',');
+		logElm.textContent = logElm.textContent + '\n' + 'doAsGS [' + str.substr(Math.floor(0 / 3), 40) + ' ' + as.substr(Math.floor(0 / 3), 40) + '] ';
+		u8as.push(b);
+	}
+	finishRGB(u8as, logElm) {
+		const u8a = Base64Util.joinU8as(u8as);
+		const str = td.decode(u8a);
+		const tokens = str.split(',');
+		if (tokens.length < 3) {
+			console.log('str:' + str);
+			alert('invalid data! Empty!');
+			return { fnb64: '', type: '', b64: '' };
+		}
+		logElm.textContent = logElm.textContent + '\n' + 'finishRGB';
+		return { fnb64: tokens.shift(), type: tokens.shift(), b64: tokens.shift() };
+	}
+	finishGS(u8as, logElm) {
+		const nU8as = [[], [], []];
+		let index = 0;
+		for (const b of u8as) {
+			let count = 0;
+			for (const ba of b) {
+				if (count > 0) {
+					index++;
+				}
+				const current = nU8as[index];
+				if (current) {
+					current.push(ba);
+					count++;
+				}
+			}
+		}
+		const u8a = Base64Util.joinU8as(nU8as[0]);
+		const str = td.decode(u8a);
+		const tokens = str.split(',');
+		if (tokens.length < 2) {
+			console.log('str:' + str);
+			alert('invalid data! Empty!');
+			return { fnb64: '', type: '', b64: '' };
+		}
+		logElm.textContent = logElm.textContent + '\n' + 'finishGS';
+		const u8a2 = Base64Util.joinU8as(nU8as[1]);
+		const u8a3 = nU8as[2].length > 0 ? (nU8as[2].byteLength ? (nU8as[2].byteLength > 4 ? nU8as[2] : null) : nU8as[2][0].byteLength > 4 ? Base64Util.joinU8as(nU8as[2]) : null) : null;
+		return { fnb64: tokens.shift(), type: tokens.shift(), b64: u8a3 ? Base64Util.u8a2b64u(u8a2) + '/' + Base64Util.u8a2b64u(u8a3) : Base64Util.u8a2b64(u8a2) };
 	}
 	clear() {
 		for (let id of this.fileIds) {
@@ -847,7 +994,8 @@ const sizeExpected = 'sizeExpected';
 const outputImageids = ['image1result', 'image2result', 'image3result', 'image4result'];
 const errorLogsImage = 'errorLogsImage';
 const redoBtnId = 'redoButton';
-new ImageBuilder(fileId, outputImageids, sizeId, sizeExpected, pwId1, sigId1, sizeId1, view, errorLogsImage, redoBtnId);
+const isGrascaleId = 'isGrascale';
+new ImageBuilder(fileId, outputImageids, sizeId, sizeExpected, pwId1, sigId1, sizeId1, view, errorLogsImage, redoBtnId, isGrascaleId);
 const fileIds = ['image1File', 'image2File', 'image3File', 'image4File'];
 const inputViewImageids = ['image1', 'image2', 'image3', 'image4'];
 const buttonId = 'FileOutput';
